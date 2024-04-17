a7e34a20e1da95a91aed5aafd84e14d3bd16deee
Merchand1se <157629323+Merchand1se@users.noreply.github.com>
2024-04-17 20:12:03 +0700

    1

---
+++ Practice/simpleapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# Импортируем созданное нами представление
+from .views import ProductsList, ProductDetail
+
+
+urlpatterns = [
+   path('', ProductsList.as_view()),
+   path('<int:pk>', ProductDetail.as_view()),
+]
+
+
---
+++ Practice/simpleapp/views.py
@@ -0,0 +1,29 @@
+from django.views.generic import ListView, DetailView
+from .models import Product
+from datetime import datetime
+from .filters import ProductFilter
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+        return context
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
